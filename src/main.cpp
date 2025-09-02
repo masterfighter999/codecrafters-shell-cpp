e1e3096f5cac1bf7f106d67e2e8a5486a36454fd
Swayam Chatterjee <masterfighter999@gmail.com>
2025-09-02 20:42:23 +0530

    type builtin: builtins

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-   std::cout << "$ ";
+  std::cout << "$ ";
 
   std::string input;
   std::getline(std::cin, input);
   if (input == "exit 0")
     return 0;
-  else if (input.find("echo") != std::string::npos) std::cout << input.substr(5) << "\n";
-  else std::cout << input << ": command not found" << std::endl;
+  else if (input.substr(0, 5) == "echo ")
+    std::cout << input.substr(5) << "\n";
+  else if (input.substr(0, 5) == "type ")
+  {
+    if (input.substr(5, 4) == "echo" || input.substr(5, 4) == "exit" || input.substr(5, 4) == "type")
+      std::cout << input.substr(5) << " is a shell builtin\n";
+
+    else
+      std::cout << input.substr(5) << ": not found\n";
+  }
+  else
+    std::cout << input << ": command not found" << std::endl;
 
   main();
 }
